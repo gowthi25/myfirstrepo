2b9cf0bbf5a2f895533550d14e2a46c99301caad
gowthi25 <narsimhaavtar@gmail.com>
2025-11-09 19:43:34 +0530

    Update app.java

--- app.java
+++ app.java
@@ -1,13 +1,5 @@
-public class VowelConsonant {
-
+public class HelloWorld {
     public static void main(String[] args) {
-
-        char ch = 'i';
-
-        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
-            System.out.println(ch + " is vowel");
-        else
-            System.out.println(ch + " is consonant");
-
+        System.out.println("Hello, World!");
     }
 }
