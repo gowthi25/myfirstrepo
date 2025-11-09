80e081b5602d7823a0b0d562950c3be7eba225f3
gowthi25 <narsimhaavtar@gmail.com>
2025-11-09 19:47:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
-python code
-we can add here
-python is pythoning
+"session": {
+        "new": true,
+        "sessionId": "amzn1.echo-api.session.xxxxxxxxxxx",
+        "application": {
+            "applicationId": "amzn1.ask.skill.xxxxxxxxxx"
+        },
+        "user": {
+            "userId": "amzn1.ask.account.xxxxxxx",
+            "accessToken": "xxxxxxxxxxxxxx"
